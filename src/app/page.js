e40c963a7e6ea4f51e4c981bf10fa997fa5406d2
Imad-Ocreateo <imad@ocreateo.com>
2024-06-01 13:38:41 +0500

    slider

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,11 @@
- 
- 
- 
- export default function Home() {
+import CustomSlider from "@/components/Home/CustomSlider";
+
+export default function Home() {
   return (
     <>
-    <div className=" h-screen bg-red-400">
-     </div>
-     <div className=" h-screen bg-red-600">
-     </div>
+      <div className=" h-screen bg-red-400"></div>
+      <div className=" h-screen bg-red-600"></div>
+      <CustomSlider />
     </>
   );
 }
--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,6 @@
 import Logo from "./images/Logo.png";
- 
+import pic1 from "./images/pic1.png";
+import pic2 from "./images/pic2.png";
+import pic3 from "./images/pic3.png";
 
-export { Logo,   };
+export { Logo, pic2, pic3, pic1 };
