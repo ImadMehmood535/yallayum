192c1e00dba48f070b9a4e3069ae4db820f9ddc4
zeeshansheikh214 <110165095+zeeshansheikh214@users.noreply.github.com>
2024-06-05 18:50:33 +0500

    custom slider fixing

--- src/components/Home/CustomSlider/CustomSlider.jsx
+++ src/components/Home/CustomSlider/CustomSlider.jsx
@@ -18,19 +18,22 @@ const CustomSlider = () => {
 
   const getClassName = (index) => {
     if (index === currentIndex) {
-      return "active z-0 -translate-x-[240%] scale-150";
+      return "active z-0 -translate-x-[240%] scale-150  hover:-translate-y-[10%]";
     }
     if (index === (currentIndex + 1) % slidesData.length) {
-      return "next  next z-10 -translate-x-[100%] scale-100";
+      return "next  next z-10 -translate-x-[90%] scale-100";
     }
     if (index === (currentIndex - 1 + slidesData.length) % slidesData.length) {
       return "preview-next next z-10  translate-x-[40%] scale-75";
     }
     return "";
   };
+  const getBackgroundClass = (index) => {
+    return `bg-image bg-item-${index}`;
+  };
 
   return (
-    <div className="CustomSlider  pageLayout   px-0 mx-auto">
+    <div className="CustomSlider pageLayout !py-20 px-0 mx-auto overflow-x-hidden">
       <div className="slider-wrapper flex justify-between items-center pl-[3%]">
         <div className="w-full md:max-w-[30%]">
           <Details
@@ -41,7 +44,7 @@ const CustomSlider = () => {
           />
         </div>
 
-        <div className="ze-image-area flex w-full justify-end items-center relative">
+        <div className={`ze-image-area flex w-full justify-end items-center relative min-h-[500px] ${getBackgroundClass(currentIndex)}`}>
           {slidesData.map((slide, index) => (
             <div key={index} className={`item absolute right-0 ${getClassName(index)}`}>
               <Image
--- src/components/layout/Ticker.jsx
+++ src/components/layout/Ticker.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Ticker = () => {
   const wordlist = ["Freeze the taste", "Buy Now"];
-  console.log("hi");
+
   return (
     <div className="Ticker bg-[#F9F1F8]">
       <div className="relative text-black py-2 overflow-hidden">
@@ -12,7 +12,9 @@ const Ticker = () => {
               <>
                 {wordlist.map((word, idx) => (
                   <React.Fragment key={`${word}-${index}-${idx}`}>
-                    <span className="text-[#3C3C3B] text-sm font-normal w-full">{word}</span>
+                    <span className="text-[#3C3C3B] text-sm font-normal w-full">
+                      {word}
+                    </span>
                     <span className="font-bold text-2xl w-full">·</span>
                   </React.Fragment>
                 ))}
