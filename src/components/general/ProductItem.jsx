ecea51f0d7a13a54ee2a7fc827a84c41538ac050
zeeshansheikh214 <110165095+zeeshansheikh214@users.noreply.github.com>
2024-06-07 16:50:52 +0500

    flip animation

--- src/components/general/ProductItem.jsx
+++ src/components/general/ProductItem.jsx
@@ -3,18 +3,22 @@ import React from "react";
 
 const ProductItem = ({ data, slider }) => {
   return (
-    <div className={`ProductItem   group flex flex-col justify-center cursor-pointer transition-all duration-200  relative  p-4  `}>
-      <div className="item-wrapper max-w-[200px] mx-auto  ">
-        <Image
-          src={data?.image}
-          alt={data?.image}
-          width={350}
-          height={350}
-          quality={100}
-          // objectFit="contain"
-          // layout="responsive"
-          className={`${slider ? "h-auto md:h-[230px] 2xl:h-auto " : "  "}`}
-        />
+    <div
+      className={`ProductItem   group flex flex-col justify-center cursor-pointer transition-all duration-200  relative  p-4  `}
+    >
+      <div className="item-wrapper max-w-[200px] mx-auto flip-container ">
+        <div className="flip-contain">
+          <Image
+            src={data?.image}
+            alt={data?.image}
+            width={350}
+            height={350}
+            quality={100}
+            // objectFit="contain"
+            // layout="responsive"
+            className={` ${slider ? "h-auto md:h-[230px] 2xl:h-auto " : "  "}`}
+          />
+        </div>
         <h4 className="text-[#3C3C3B] GeneralSans font-normal text-lg text-center whitespace-nowrap">
           {data?.title}
         </h4>
