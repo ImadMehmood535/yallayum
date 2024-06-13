55fad2a43788a94975adb7703c9e9863670c88ed
Imad-Ocreateo <imad@ocreateo.com>
2024-06-13 13:49:38 +0500

    test

--- src/app/page.js
+++ src/app/page.js
@@ -25,7 +25,7 @@ export default function Home() {
       <Blogs blogsData={blogsData} />
       <ProductFeature />
       <FeedSlider feedlist={feedlist} />
-      <StoreFeatures/>
+      {/* <StoreFeatures/> */}
     </>
   );
 }
