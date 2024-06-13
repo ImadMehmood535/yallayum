ba2dfe65b252b1ed4cbb7aa7084839fe357b663d
Imad-Ocreateo <imad@ocreateo.com>
2024-06-13 12:46:57 +0500

    remove

--- src/app/page.js
+++ src/app/page.js
@@ -1,30 +1,9 @@
- 
-import { blogsData } from "@/data/blogsData";
-import { feedlist } from "@/data/feedlist";
-import { reviewData } from "@/data/reviewData";
-import Blogs from "@/components/Home/Blogs";
 import Banner from "@/components/Home/Banner";
-import FeatureProduct from "@/components/Home/FeatureProduct";
-import OurImpact from "@/components/Home/OurImpact";
-import CustomSlider from "@/components/Home/CustomSlider/CustomSlider";
-import InstaFeeds from "@/components/Home/InstaFeeds";
-import ProductFeature from "@/components/general/ProductFeature";
-import FeedSlider from "@/components/Home/FeedSlider";
-import { productData } from "@/data/productData";
-import StoreFeatures from "@/components/general/StoreFeatures";
 
 export default function Home() {
   return (
     <>
       <Banner />
-      <FeatureProduct data={productData} />
-      <OurImpact />
-      <CustomSlider />
-      <InstaFeeds reviewData={reviewData} />
-      <Blogs blogsData={blogsData} />
-      <ProductFeature />
-      <FeedSlider feedlist={feedlist} />
-      <StoreFeatures/>
     </>
   );
 }
