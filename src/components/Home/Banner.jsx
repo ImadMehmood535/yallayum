b367936c12bb331b143b5b116976c37aba60a1b5
Imad-Ocreateo <imad@ocreateo.com>
2024-06-13 10:53:26 +0500

    test

--- src/components/Home/Banner.jsx
+++ src/components/Home/Banner.jsx
@@ -1,16 +1,16 @@
 "use client";
 import React from "react";
-import dynamic from 'next/dynamic';
+import dynamic from "next/dynamic";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Autoplay } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
 import Image from "next/image";
 import { homeBannerSlide } from "@/data/slides";
-import Head from 'next/head';
+import Head from "next/head";
 
 // Dynamically import the AnimatedButton to defer its loading
-const AnimatedButton = dynamic(() => import('../general/AnimatedButton'), {
+const AnimatedButton = dynamic(() => import("../general/AnimatedButton"), {
   ssr: false,
 });
 
@@ -18,11 +18,10 @@ const Banner = () => {
   let slidesData = [...homeBannerSlide];
   return (
     <div className="Banner">
- 
       <Swiper
         pagination={{
           dynamicBullets: true,
-          clickable: true
+          clickable: true,
         }}
         loop={true}
         autoplay={{
@@ -51,20 +50,38 @@ const Banner = () => {
                 <div className="min-h-[630px] flex flex-col md:flex-row justify-between items-end gap-2 md:px-8 pt-16 md:pt-8">
                   <div className="w-full md:w-2/4 content-area">
                     {slide?.title ? (
-                      <div className="pb-[15%]  ">
-                        <h1 className="font-bold mb-6 Fedra-700">{slide?.title}</h1>
+                      <div className="pb-[15%]">
+                        <h1 className="font-bold mb-6 Fedra-700">
+                          {slide?.title}
+                        </h1>
                         <p className="max-w-[560px] mb-6 GeneralSans">
                           {slide?.description}
                         </p>
                         <AnimatedButton text={"BUY NOW"} shop={true} />
                       </div>
                     ) : (
-                     null
+                      <Image
+                        src={slide?.firstimage}
+                        alt={"Slide Image"}
+                        width={630}
+                        height={540}
+                        quality={100}
+                        className="object-contain w-auto h-auto"
+                        priority // Ensure key images are loaded eagerly
+                      />
                     )}
                   </div>
 
                   <div className="w-full md:max-w-[570px] img-area">
-                    null
+                    <Image
+                      src={slide?.imageSrc}
+                      alt={"Banner Image"}
+                      width={800}
+                      height={300}
+                      quality={100}
+                      className="w-full h-full"
+                      priority // Ensure key images are loaded eagerly
+                    />
                   </div>
                 </div>
               </div>
