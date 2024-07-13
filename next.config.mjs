6d0836e9a46436861a4980be1aa34555ab56f36b
Imad-Ocreateo <imad@ocreateo.com>
2024-07-14 01:51:32 +0500

    image

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["yalla.ctround.com" , "10.13.1.76"],
+    domains: ["yalla.ctround.com" , "10.13.1.76" , "seal-app-ck8z8.ondigitalocean.app"],
   },
 };
 
