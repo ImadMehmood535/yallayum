0fd8091668d16f126dbeee1298d9d61c60e76d2f
Imad-Ocreateo <imad@ocreateo.com>
2024-08-02 12:37:47 +0500

    shipping policy and terms

---
+++ src/app/terms-and-conditions/page.js
@@ -0,0 +1,91 @@
+import InnerpageHeader from "@/components/general/InnerpageHeader";
+import React from "react";
+
+const Index = () => {
+  return (
+    <div>
+      <div className="w-full ">
+        <InnerpageHeader pagetitle={"Return and Shipping Policy"} />
+      </div>
+
+      <div className="py-12">
+        <div className="container flex flex-col gap-8 ">
+          <p className="font-medium">
+            These terms and conditions outline the rules and regulations for the
+            use of YallaYum’s Website.
+          </p>
+          <p>
+            Welcome to yallayum.ae. The website is operated by YallaYum. If you
+            continue to browse and use this website, you are agreeing to comply
+            with and be bound by the following terms and conditions of use,
+            which together with our privacy policy govern companies’
+            relationship with you in relation to this website. If you disagree
+            with any part of these terms and conditions, please do not use our
+            website.
+          </p>
+
+          <p>
+            The use of this website is subject to the following terms of use:
+          </p>
+
+          <p>
+            The content of the pages of this website is for your general
+            information and use only. It is subject to change without notice.
+          </p>
+
+          <p>
+            Neither we nor any third parties provide any warranty or guarantee
+            as to the accuracy, timeliness, performance, completeness or
+            suitability of the information and materials found or offered on
+            this website for any particular purpose. You acknowledge that such
+            information and materials may contain inaccuracies or errors and we
+            expressly exclude liability for any such inaccuracies or errors to
+            the fullest extent permitted by law.
+          </p>
+
+          <p>
+            Your use of any information or materials on this website is entirely
+            at your own risk, for which we shall not be liable. It shall be your
+            own responsibility to ensure that any products, services or
+            information available through this website meet your specific
+            requirements.
+          </p>
+
+          <p>
+            This website contains material which is owned by or licensed to us.
+            This material includes, but is not limited to, the design, layout,
+            look, appearance and graphics. Reproduction is prohibited other than
+            in accordance with the copyright notice, which forms part of these
+            terms and conditions.
+          </p>
+
+          <p>
+            All trademarks reproduced in this website, which are not the
+            property of, or licensed to the operator, are acknowledged on the
+            website.
+          </p>
+
+          <p>
+            Unauthorized use of this website may give rise to a claim for
+            damages and/or be a criminal offense.
+          </p>
+
+          <p>
+            Promotions or offers listed on website – Some offers may have
+            limited time to expired so make sure to check the offers expiry date
+          </p>
+
+          <p>
+            From time to time, this website may also include links to other
+            websites. These links are provided for your convenience to provide
+            further information. They do not signify that we endorse the
+            website(s). We have no responsibility for the content of the linked
+            website(s).
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Index;
