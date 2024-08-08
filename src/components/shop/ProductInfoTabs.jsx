a3d01ae4e85a95a6ff2e57271e0a358712336549
Imad-Ocreateo <imad@ocreateo.com>
2024-08-08 11:13:11 +0500

    ingredeints data

--- src/components/shop/ProductInfoTabs.jsx
+++ src/components/shop/ProductInfoTabs.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import Tiptap from "../general/TipTap";
-// import Editor from "../general/Editor";
-// import { Emoji } from "react-emojis";
+import Image from "next/image";
 
 const ProductInfoTabs = ({ data }) => {
   const [activeTab, setActiveTab] = useState(1);
@@ -9,6 +8,7 @@ const ProductInfoTabs = ({ data }) => {
   const handleTabClick = (tabID) => {
     setActiveTab(tabID);
   };
+
   const replaceIconCodesWithEmojis = (text) => {
     return text.replace(/\[`(U\+\w+)\`]/g, (match, p1) => {
       const codePoint = parseInt(p1.replace("U+", ""), 16);
@@ -21,7 +21,7 @@ const ProductInfoTabs = ({ data }) => {
   const processedIntegrations = replaceIconCodesWithEmojis(data.ingredients);
 
   return (
-    <div className="product-info-tabs">
+    <div className="product-info-tabs w-full">
       <div className="wrapper">
         <div className="tab-wrapper mb-4">
           <ul className="tabs flex justify-start GeneralSans font-bold gap-5">
@@ -44,7 +44,7 @@ const ProductInfoTabs = ({ data }) => {
           </ul>
         </div>
 
-        <div className="content-wrapper">
+        <div className="content-wrapper w-full">
           <div
             className={`tab-content GeneralSans ${
               activeTab === 1 ? "active" : ""
@@ -58,8 +58,30 @@ const ProductInfoTabs = ({ data }) => {
               activeTab === 2 ? "active" : ""
             }`}
           >
-            <div className=" ">
+            <div className="mb-8 w-full">
               <Tiptap content={processedIntegrations} />
+            </div>
+
+            <div className="mt-8 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6 md:gap-8 justify-evenly w-full">
+              {data?.ingredientsData?.map((item, index) => (
+                <div
+                  className="flex flex-col items-center text-center"
+                  key={index}
+                >
+                  <div className="mb-4">
+                    <Image
+                      src={item?.imageUrl}
+                      alt={item?.name}
+                      quality={100}
+                      className="w-12 h-12 object-contain"
+                      height={64}
+                      width={64}
+                    />
+                  </div>
+                  <h3 className="text-lg font-semibold mb-2">{item?.head}</h3>
+                  <p className="text-sm text-gray-600">{item?.paragh}</p>
+                </div>
+              ))}
             </div>
           </div>
         </div>
